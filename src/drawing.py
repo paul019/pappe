fd9709795303b2d249c99a895c5b7f81b9d401b4
Splines <dominic-plein@gmx.de>
2023-08-31 01:17:38 +0200

    Prettify drawing (also use global variables)

--- src/drawing.py
+++ src/drawing.py
@@ -3,6 +3,11 @@ from pdf_annotate import PdfAnnotator, Location, Appearance
 from measurement import Measurement
 from transformer import Axis, Transformer
 
+INDICATOR_COLOR = (1, 0, 0)
+AXIS_COLOR = (0, 0, 0)
+INDICATOR_STROKE_WIDTH = 0.5
+AXIS_STROKE_WIDTH = 1.0
+
 
 class Drawer:
     """
@@ -61,47 +66,41 @@ class Drawer:
             m.x, m.y - m.lower_error)
 
         # vertical line
-        self.a.add_annotation(
-            'line',
-            Location(points=[coords_bottom, coords_top], page=0),
-            Appearance(stroke_color=(1, 0, 0), stroke_width=0.5)
-        )
+        location = Location(points=[coords_bottom, coords_top], page=0)
+        self.a.add_annotation('line', location,
+                              Appearance(stroke_color=INDICATOR_COLOR,
+                                         stroke_width=INDICATOR_STROKE_WIDTH))
 
         # lower line
         points = [(coords_bottom[0]-self.cross_size/2, coords_bottom[1]),
                   (coords_bottom[0]+self.cross_size/2, coords_bottom[1])]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(1, 0, 0), stroke_width=0.5)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=INDICATOR_COLOR,
+                                         stroke_width=INDICATOR_STROKE_WIDTH))
 
         # upper line
         points = [(coords_top[0]-self.cross_size/2, coords_top[1]),
                   (coords_top[0]+self.cross_size/2, coords_top[1])]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(1, 0, 0), stroke_width=0.5)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=INDICATOR_COLOR,
+                                         stroke_width=INDICATOR_STROKE_WIDTH))
 
     def _draw_datapoint(self, m: Measurement):
         coords = self.trafo.get_pdf_coords_from_data_point(m.x, m.y)
+
+        # One side of cross
         points = [(coords[0]-self.cross_size/2, coords[1]-self.cross_size/2),
                   (coords[0]+self.cross_size/2, coords[1]+self.cross_size/2)]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(1, 0, 0), stroke_width=0.5)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=INDICATOR_COLOR,
+                                         stroke_width=INDICATOR_STROKE_WIDTH))
 
+        # Other side of cross
         points = [(coords[0]+self.cross_size/2, coords[1]-self.cross_size/2),
                   (coords[0]-self.cross_size/2, coords[1]+self.cross_size/2)]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(1, 0, 0), stroke_width=0.5)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=INDICATOR_COLOR,
+                                         stroke_width=INDICATOR_STROKE_WIDTH))
 
     def _draw_vertical_axis_number(self, num_grid):
         num_data = self.trafo.grid_coord_to_data_label(num_grid, Axis.VERTICAL)
@@ -110,23 +109,21 @@ class Drawer:
         coords = self.trafo.get_pdf_coords_for_point_on_axis(
             Axis.VERTICAL, num_grid)
 
+        # Line
         points = [(coords[0]-self.axis_tick_size/2, coords[1]),
                   (coords[0]+self.axis_tick_size/2, coords[1])]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(0, 0, 0), stroke_width=1)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=AXIS_COLOR,
+                                         stroke_width=AXIS_STROKE_WIDTH))
 
+        # Text
         x1, y1 = coords[0] - 200, coords[1] - 50
         x2, y2 = coords[0] - self.axis_tick_size/2, coords[1] + 50
-        self.a.add_annotation(
-            'text',
-            Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0),
-            Appearance(content=label, fill=[0, 0, 0],
-                       stroke_width=1,
-                       font_size=40, text_align='right')
-        )
+        location = Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0)
+        self.a.add_annotation('text', location,
+                              Appearance(content=label, fill=[0, 0, 0],
+                                         stroke_width=1,
+                                         font_size=40, text_align='right'))
 
     def _draw_horizontal_axis_number(self, num_grid):
         num_data = self.trafo.grid_coord_to_data_label(
@@ -136,23 +133,21 @@ class Drawer:
         coords = self.trafo.get_pdf_coords_for_point_on_axis(
             Axis.HORIZONTAL, num_grid)
 
+        # Line
         points = [(coords[0], coords[1] - self.axis_tick_size/2),
                   (coords[0], coords[1] + self.axis_tick_size/2)]
-        self.a.add_annotation(
-            'line',
-            Location(points=points, page=0),
-            Appearance(stroke_color=(0, 0, 0), stroke_width=1)
-        )
+        self.a.add_annotation('line', Location(points=points, page=0),
+                              Appearance(stroke_color=AXIS_COLOR,
+                                         stroke_width=AXIS_STROKE_WIDTH))
 
+        # Text
         x1, y1 = coords[0]-100, coords[1]-100-self.axis_tick_size/2
         x2, y2 = coords[0]+100, coords[1]-self.axis_tick_size/2
-        self.a.add_annotation(
-            'text',
-            Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0),
-            Appearance(content=label, fill=[0, 0, 0],
-                       stroke_width=1,
-                       font_size=40, text_align='center')
-        )
+        location = Location(x1=x1, y1=y1, x2=x2, y2=y2, page=0)
+        self.a.add_annotation('text', location,
+                              Appearance(content=label, fill=[0, 0, 0],
+                                         stroke_width=1,
+                                         font_size=40, text_align='center'))
 
     def _draw_axis(self, axis: Axis):
         if axis == Axis.HORIZONTAL:
@@ -165,8 +160,7 @@ class Drawer:
         coords_end = self.trafo.get_pdf_coords_for_point_on_axis(
             axis, num_blocks)
 
-        self.a.add_annotation(
-            'line',
-            Location(points=[coords_start, coords_end], page=0),
-            Appearance(stroke_color=(0, 0, 0), stroke_width=1)
-        )
+        location = Location(points=[coords_start, coords_end], page=0)
+        self.a.add_annotation('line', location,
+                              Appearance(stroke_color=AXIS_COLOR,
+                                         stroke_width=AXIS_STROKE_WIDTH))
