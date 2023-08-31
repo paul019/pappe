3dadb66fbe37bc81f1c5444adb9ea93111a09e40
Splines <dominic-plein@gmx.de>
2023-08-31 02:15:20 +0200

    Add CLI argparser

--- src/pappe.py
+++ src/pappe.py
@@ -1,26 +1,40 @@
+import argparse
+
 from pappe_parser import parse_csv, parse_config
 from drawer import Drawer
 from transformer import Transformer
 
 
 def main():
-    config_path = 'config.toml'
-    config = parse_config(config_path)
+    # Parse CLI arguments
+    cli_parser = argparse.ArgumentParser(
+        prog='pappe',
+        description='A CLI to draw your data on top of millimeter paper.',
+        add_help=True)
+    cli_parser.add_argument('input', help='path to input CSV file')
+    cli_parser.add_argument('output', help='path to output pdf')
+    cli_parser.add_argument('-c', '--config',
+                            help='path to TOML config file',
+                            default='config.toml')
+    args = cli_parser.parse_args()
 
+    # Master config
+    config = parse_config(args.config)
     grid_no: int = config['grid_variant']
     config_path_grid = f'grids/grid{grid_no}.toml'
     complete_grid_config = parse_config(config_path_grid)
 
-    csv_path = 'input/data.csv'  # TODO: will become CLI param
-    measurements = parse_csv(csv_path)
+    # Parse input CSV
+    measurements = parse_csv(args.input)
 
+    # Setup drawing
     trafo = Transformer(complete_grid_config['grid'],
                         config['factors'], config['origins'])
     drawer = Drawer(trafo, complete_grid_config)
     drawer.draw_all(measurements)
 
-    output_path = 'output/output.pdf'  # TODO: will become CLI param
-    drawer.save(output_path)
+    # Output
+    drawer.save(args.output)
 
 
 if __name__ == '__main__':
